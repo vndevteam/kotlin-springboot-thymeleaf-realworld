212f94093323031e480b1b0f947ce7924a8257cf
Lam Ngoc Khuong <me@ngockhuong.com>
2023-09-16 22:07:19 +0700

    Add dependences

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    id("org.springframework.boot") version "2.7.15"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.9.0"
-    application
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "vn.vndevteam"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.0.4")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    developmentOnly("org.springframework.boot:spring-boot-devtools:3.0.4")
+    runtimeOnly("com.mysql:mysql-connector-j:8.0.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
 }
 
-tasks.test {
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(8)
-}
-
-application {
-    mainClass.set("MainKt")
 }
---
+++ src/main/kotlin/vn/vndevteam/realworld/RealWorldApplication.kt
@@ -0,0 +1,11 @@
+package vn.vndevteam.realworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealWorldApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealWorldApplication>(*args)
+}
